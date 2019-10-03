a7fcdf5b5966574d5788d5baa0f72a5083fb021f
iambeone <iambeone@gmail.com>
2019-10-03 17:10:35 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-machine create --driver digitalocean --digitalocean-access-token $token docker-sandbox-$name
 docker-machine env docker-sandbox-$name --shell sh && eval $(docker-machine env docker-sandbox-$name --shell sh)
-docker load < dockerimage.tgz
+docker load < /github/workflow/dockerimage.tgz
 eval $(docker run -d -p $ports dockerimage)
