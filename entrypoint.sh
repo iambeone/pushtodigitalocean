9abe5d117e6d09115512f1ce17137c8487a2f7e5
iambeone <iambeone@gmail.com>
2019-10-04 09:28:19 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 docker-machine create --driver digitalocean --digitalocean-access-token $token $name
 docker-machine env $name --shell sh && eval $(docker-machine env $name --shell sh)
-ls -la
 docker load < $file
-docker image ls
-eval $(docker run -d -p $ports $image)
+docker run -d -p $ports $image
