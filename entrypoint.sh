d33e2a211c88fc83311d6ecd71bfd6e65b1e5d97
iambeone <iambeone@gmail.com>
2019-10-04 09:41:30 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@ docker-machine create --driver digitalocean --digitalocean-access-token $token $
 docker-machine env $name --shell sh && eval $(docker-machine env $name --shell sh)
 docker load < $file
 docker run -d -p $ports $image
+ip=$(docker-machine ip $name)
+echo ::set-output name=id::$ip
