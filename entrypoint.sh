ccafa0b9566213578ea988680ec3044e86c9819d
iambeone <iambeone@gmail.com>
2019-10-08 09:00:09 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# adding ssh key
-mkdir -p ~/.ssh
-echo "$private_key" | tr -d '\r' > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
 docker-machine create --driver digitalocean \
   --digitalocean-ssh-key-fingerprint $fingerprint \
   --digitalocean-monitoring \
